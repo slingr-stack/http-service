7e70ef0df2e7661920f1b03591ef0b6787e611a5
Alejandro Greggio <agreggio@slingr.io>
2022-12-22 17:05:25 -0300

    #13118 Resolves comments

--- flowSteps/httpCall/step.js
+++ flowSteps/httpCall/step.js
@@ -11,9 +11,12 @@
  * @param {boolean} followRedirects, This is used to config follow redirects.
  * @param {boolean} download, This is used to config download.
  * @param {boolean} fullResponse, This is used to config full response.
+ * @param {number} connectionTimeout, Read timeout interval, in milliseconds.
+ * @param {number} readTimeout, Connect timeout interval, in milliseconds.
  */
 step.httpCall = function (method, url, headers, params, body, requiresCallBack,
-						  callbackData, callbacks,settings ,followRedirects, download, fullResponse) {
+						  callbackData, callbacks,settings ,followRedirects,
+						  download, fullResponse,connectionTimeout, readTimeout) {
 
 	headers = isObject(headers) ? headers : stringToObject(headers)
 	params = isObject(params) ? params : stringToObject(params)
@@ -27,7 +30,9 @@ step.httpCall = function (method, url, headers, params, body, requiresCallBack,
 		settings : {
 			followRedirects : followRedirects,
 			download : download,
-			fullResponse : fullResponse
+			fullResponse : fullResponse,
+			connectionTimeout:connectionTimeout,
+			readTimeout:readTimeout
 		}
 	}
 
